b8c318513199c5956210c46d6bbd5d99641165ff
Patrick <patrickmo@gmx.de>
2015-07-24 15:49:37 +0200

    - add rgb states only for color lights - set all known states immediately - use bri for on/off - change behaviour of bri and on - complete rewrite of stateChange

--- hue.js
+++ hue.js
@@ -17,67 +17,190 @@ var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var adapter = utils.adapter('hue');
 
 adapter.on('stateChange', function (id, state) {
-    if (id && state && !state.ack) {
-        adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-        var tmp = id.split('.');
-        var dp  = tmp.pop();
-        id      = tmp.slice(2).join('.');
-        var ls = {};
-        ls[dp] = state.val;
-        if(dp == 'r' || dp == 'g' || dp == 'b') {
-            var rgb = {};
-            adapter.getState([id, 'r'].join('.'), function (err, astate){
-                rgb['r'] = Math.round(astate.val);
-            });
-            adapter.getState([id, 'g'].join('.'), function (err, astate) {
-                rgb['g'] = Math.round(astate.val);
-            });
-            adapter.getState([id, 'b'].join('.'), function (err, astate) {
-                rgb['b'] = Math.round(astate.val);
-                var brightness = Math.min(254,Math.max(rgb.r,rgb.g,rgb.b));
-                var huestate = hue.lightState.create().on().rgb(rgb.r,rgb.g,rgb.b).bri(brightness);
-                api.setLightState(channelIds[id], huestate, function (err, res) {
-                    if (!err && res) {
-                        adapter.setState([id, dp].join('.'), {val: Math.round(state.val), ack: true});
-                        if (brightness == 0) {
-                            var huestate = hue.lightState.create().off();
-                            api.setLightState(channelIds[id], huestate, function (err, res) {
-                                if (!err && res) {
-                                    adapter.setState([id, 'on'].join('.'), {val: false, ack: true});
-                                }
-                            });
-                        }else {
-                            adapter.setState([id, 'on'].join('.'), {val: true, ack: true});
-                        }
-                        adapter.setState([id, 'bri'].join('.'), {val: brightness, ack: true});
-                    }
-                });
-            });
-        }else{
-            if (dp == 'ct') {
-                ls['on'] = true;
-            }
-            if (dp == 'bri' && state.val > 0) {
-                ls['on'] = true;
-            }else if (dp == 'bri' && state.val == 0){
-                ls['on'] = false;
-            }
-            api.setLightState(channelIds[id], ls, function (err, res) {
-                if (!err && res) {
-                    adapter.setState([id, dp].join('.'), {val: state.val, ack: true});
-                    if (dp == 'bri') {
-                        adapter.setState([id, 'on'].join('.'), {val: ls.on, ack: true});
-                    }else if (dp == 'on' && state.val == true) {
-                        adapter.getState([id, 'bri'].join('.'), function (err, astate) {
-                            if (astate.val < 5) {
-                                adapter.setState([id, 'bri'].join('.'), {val: 5, ack: true});
-                            }
-                        });
-                    }
-                }
-            });
-        }
+    if (!id || !state || state.ack) {
+        return;
     }
+
+    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
+    var tmp = id.split('.');
+    var dp  = tmp.pop();
+    id      = tmp.slice(2).join('.');
+    var ls = {};
+    //if .on changed to true instead change .bri to 254 or 0
+    if (dp == 'on') {
+        var bri = state.val ? 254 : 0;
+        adapter.setState([id, 'bri'].join('.'), {val: bri, ack: false});
+        return;
+    }
+    adapter.getStates(id + '.*', function (err, idStates){
+        if (err) {
+            adapter.log.error(err);
+            return;
+        }
+        var ls = {};
+        for (var idState in idStates) {
+            var idtmp = idState.split('.');
+            var iddp  = idtmp.pop();
+            switch (iddp) {
+                case 'bri':
+                    ls[iddp] = idStates[idState].val;
+                    break;
+                case 'alert':
+                    if (dp == 'alert') ls[iddp] = idStates[idState].val;
+                    break;
+                case 'effect':
+                    if (dp == 'effect') ls[iddp] = idStates[idState].val;
+                    break;
+                case 'r':
+                case 'g':
+                case 'b':
+                    if (dp == 'r' || dp == 'g' || dp == 'b'){
+                        ls[iddp] = idStates[idState].val;
+                        ls['colormode'] = 'xy';
+                    }
+                    break;
+                case 'ct':
+                    if (dp == 'ct') {
+                        ls[iddp] = idStates[idState].val;
+                        ls['colormode'] = 'ct';
+                    }
+                    break;
+                case 'hue':
+                case 'sat':
+                    if (dp == 'hue' || dp == 'sat'){
+                        ls[iddp] = idStates[idState].val;
+                        ls['colormode'] = 'hs';
+                    }
+                    break;
+                case 'xy':
+                    if (dp == 'xy') {
+                        ls[iddp] = idStates[idState].val;
+                        ls['colormode'] = 'xy';
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        //apply rgb to xy
+        if ('r' in ls) {
+            var xyb = calculateXYB({'r': ls.r/255, 'g': ls.g/255, 'b': ls.b/255});
+            ls.bri = Math.max(ls.r,ls.g,ls.b);
+            ls.xy = xyb.x + ',' + xyb.y;
+        }
+
+        //create lightState from ls
+        //and check values
+        var lightState = hue.lightState.create();
+        var finalLS = {};
+        if (ls.bri > 0) {
+            lightState = lightState.on();
+            lightState = lightState.bri(Math.min(254,ls.bri));
+            finalLS['bri'] = Math.min(254,ls.bri);
+            finalLS['on'] = true;
+        }else {
+            lightState = lightState.bri(0);
+            lightState = lightState.off();
+            finalLS['bri'] = 0;
+            finalLS['on'] = false;
+        }
+        if ('xy' in ls) {
+            var xy = ls.xy.split(',');
+            xy = {'x': xy[0], 'y': xy[1]};
+            if (!checkPointInLampsReach(xy,colorPointsForModel(''))) {
+                var colorPoints = colorPointsForModel('');
+                //It seems the colour is out of reach
+                //let's find the closest colour we can produce with our lamp and send this XY value out.
+
+                //Find the closest point on each line in the triangle.
+                var pAB = getClosestPointToPoints(colorPoints.r,colorPoints.g,xy);
+                var pAC = getClosestPointToPoints(colorPoints.b,colorPoints.r,xy);
+                var pBC = getClosestPointToPoints(colorPoints.g,colorPoints.b,xy);
+
+                //Get the distances per point and see which point is closer to our Point.
+                var dAB = getDistanceBetweenTwoPoints(xy,pAB);
+                var dAC = getDistanceBetweenTwoPoints(xy,pAC);
+                var dBC = getDistanceBetweenTwoPoints(xy,pBC);
+
+                var lowest = dAB;
+                var closestPoint = pAB;
+
+                if (dAC < lowest) {
+                    lowest = dAC;
+                    closestPoint = pAC;
+                }
+                if (dBC < lowest) {
+                    lowest = dBC;
+                    closestPoint = pBC;
+                }
+
+                //Change the xy value to a value which is within the reach of the lamp.
+                xy.x = closestPoint.x;
+                xy.y = closestPoint.y;
+            }
+            finalLS['xy'] = xy.x + ',' + xy.y;
+            lightState = lightState.xy(xy.x,xy.y);
+            if (!'r' in ls && (!'bri' in ls || ls.bri == 0)) {
+                lightState = lightState.on();
+                lightState = lightState.bri(254);
+                finalLS['bri'] = 254;
+                finalLS['on'] = true;
+            }
+        }
+        if ('ct' in ls) {
+            finalLS['ct'] = Math.max(153,Math.min(500,ls.ct));
+            lightState = lightState.ct(finalLS.ct);
+            if (!'bri' in ls || ls.bri == 0) {
+                lightState = lightState.on();
+                lightState = lightState.bri(254);
+                finalLS['bri'] = 254;
+                finalLS['on'] = true;
+            }
+        }
+        if ('hue' in ls) {
+            finalLS['hue'] = Math.max(0,Math.min(65535,ls.hue))
+            lightState = lightState.hue(finalLS.hue);
+            if (!'bri' in ls || ls.bri == 0) {
+                lightState = lightState.on();
+                lightState = lightState.bri(254);
+                finalLS['bri'] = 254;
+                finalLS['on'] = true;
+            }
+        }
+        if ('sat' in ls) {
+            finalLS['sat'] = Math.max(0,Math.min(254,ls.sat));
+            lightState = lightState.hue(finalLS.sat);
+            if (!'bri' in ls || ls.bri == 0) {
+                lightState = lightState.on();
+                lightState = lightState.bri(254);
+                finalLS['bri'] = 254;
+                finalLS['on'] = true;
+            }
+        }
+        if ('alert' in ls) {
+            if (['select','lselect'].indexOf(ls.alert) == -1) finalLS['alert'] = 'none';
+            else finalLS['alert'] = ls.alert;
+            lightState = lightState.alert(finalLS.alert);
+        }
+        if ('colormode' in ls) {
+            finalLS['colormode'] = ls.colormode;
+        }
+        adapter.log.info('final lightState: ' + JSON.stringify(finalLS));
+
+        //set lightState
+        api.setLightState(channelIds[id], lightState, function (err, res) {
+            if (err || !res) {
+                adapter.log.error('error: ' + err);
+                return;
+            }
+
+            //write back known states
+            for (var finalState in finalLS) {
+                adapter.setState([id, finalState].join('.'), {val: finalLS[finalState], ack: true});
+            }
+        });
+
+    });
 });
 
 adapter.on('message', function (obj) {
@@ -171,9 +294,11 @@ function main() {
             pollIds.push(id);
             pollChannels.push(channelName);
 
-            light.state.r = 0;
-            light.state.g = 0;
-            light.state.b = 0;
+            if (light.type !== 'Dimmable plug-in unit' && light.type !== 'Dimmable light') {
+                light.state.r = 0;
+                light.state.g = 0;
+                light.state.b = 0;
+            }
 
             for (var state in light.state) {
 
@@ -196,8 +321,7 @@ function main() {
                 switch (state) {
                     case 'on':
                         obj.common.type = 'boolean';
-                        obj.common.role = 'indicator.on';
-                        obj.common.write = false;
+                        obj.common.role = 'switch';
                         break;
                     case 'bri':
                         obj.common.type = 'number';
@@ -343,3 +467,228 @@ function pollSingle() {
         });
     }
 }
+
+
+function colorPointsForModel(model)  {
+    return {
+        'r': {'x': 0.674, 'y': 0.322},
+        'g': {'x': 0.408, 'y': 0.517},
+        'b': {'x': 0.168, 'y': 0.041}
+    }
+}
+
+function calculateXYB(rgb) {
+    var red = rgb.r;
+    var green = rgb.g;
+    var blue = rgb.b;
+
+    // Apply gamma correction
+    var r = (red   > 0.04045) ? Math.pow((red   + 0.055) / (1.0 + 0.055), 2.4) : (red   / 12.92);
+    var g = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
+    var b = (blue  > 0.04045) ? Math.pow((blue  + 0.055) / (1.0 + 0.055), 2.4) : (blue  / 12.92);
+
+    // Wide gamut conversion D65
+    var X = r * 0.664511 + g * 0.154324 + b * 0.162028;
+    var Y = r * 0.283881 + g * 0.668433 + b * 0.047685;
+    var Z = r * 0.000088 + g * 0.072310 + b * 0.986039;
+
+    var cx = X / (X + Y + Z);
+    var cy = Y / (X + Y + Z);
+
+    if (isNaN(cx)) {
+        cx = 0.0;
+    }
+
+    if (isNaN(cy)) {
+        cy = 0.0;
+    }
+
+    var xyPoint = {'x': cx, 'y': cy};
+    var colorPoints = colorPointsForModel('');
+    var inReachOfLamps = checkPointInLampsReach(xyPoint, colorPoints);
+
+    if (inReachOfLamps) {
+        return {'x': cx, 'y': cy, 'b': Y};    //Math.max(rgb.r,rgb.g,rgb.b)
+    }
+
+    //It seems the colour is out of reach
+    //let's find the closest colour we can produce with our lamp and send this XY value out.
+
+    //Find the closest point on each line in the triangle.
+    var pAB = getClosestPointToPoints(colorPoints.r,colorPoints.g,xyPoint);
+    var pAC = getClosestPointToPoints(colorPoints.b,colorPoints.r,xyPoint);
+    var pBC = getClosestPointToPoints(colorPoints.g,colorPoints.b,xyPoint);
+
+    //Get the distances per point and see which point is closer to our Point.
+    var dAB = getDistanceBetweenTwoPoints(xyPoint,pAB);
+    var dAC = getDistanceBetweenTwoPoints(xyPoint,pAC);
+    var dBC = getDistanceBetweenTwoPoints(xyPoint,pBC);
+
+    var lowest = dAB;
+    var closestPoint = pAB;
+
+    if (dAC < lowest) {
+        lowest = dAC;
+        closestPoint = pAC;
+    }
+    if (dBC < lowest) {
+        lowest = dBC;
+        closestPoint = pBC;
+    }
+
+    //Change the xy value to a value which is within the reach of the lamp.
+    cx = closestPoint.x;
+    cy = closestPoint.y;
+
+    return {'x': cx, 'y': cy, 'b': Y};
+}
+
+function colorFromXYB(xyb) {
+    var xy = {'x': xyb.x, 'y': xyb.y};
+    var colorPoints = colorPointsForModel('');
+    var inReachOfLamps = checkPointInLampsReach(xy,colorPoints);
+
+    if (!inReachOfLamps) {
+        //It seems the colour is out of reach
+        //let's find the closest colour we can produce with our lamp and send this XY value out.
+
+        //Find the closest point on each line in the triangle.
+        var pAB = getClosestPointToPoints(colorPoints.r,colorPoints.g,xy);
+        var pAC = getClosestPointToPoints(colorPoints.b,colorPoints.r,xy);
+        var pBC = getClosestPointToPoints(colorPoints.g,colorPoints.b,xy);
+
+        //Get the distances per point and see which point is closer to our Point.
+        var dAB = getDistanceBetweenTwoPoints(xy,pAB);
+        var dAC = getDistanceBetweenTwoPoints(xy,pAC);
+        var dBC = getDistanceBetweenTwoPoints(xy,pBC);
+
+        var lowest = dAB;
+        var closestPoint = pAB;
+
+        if (dAC < lowest) {
+            lowest = dAC;
+            closestPoint = pAC;
+        }
+        if (dBC < lowest) {
+            lowest = dBC;
+            closestPoint = pBC;
+        }
+
+        //Change the xy value to a value which is within the reach of the lamp.
+        xy.x = closestPoint.x;
+        xy.y = closestPoint.y;
+    }
+    var x = xy.x;
+    var y = xy.y;
+    var z = 1.0 - x - y;
+
+    var Y = xyb.b;
+    var X = (Y / y) * x;
+    var Z = (Y / y) * z;
+
+    // sRGB D65 conversion
+    var r =  X * 1.656492 - Y * 0.354851 - Z * 0.255038;
+    var g = -X * 0.707196 + Y * 1.655397 + Z * 0.036152;
+    var b =  X * 0.051713 - Y * 0.121364 + Z * 1.011530;
+
+    if (r > b && r > g && r > 1.0) {
+        // red is too big
+        g = g / r;
+        b = b / r;
+        r = 1.0;
+    }
+    else if (g > b && g > r && g > 1.0) {
+        // green is too big
+        r = r / g;
+        b = b / g;
+        g = 1.0;
+    }
+    else if (b > r && b > g && b > 1.0) {
+        // blue is too big
+        r = r / b;
+        g = g / b;
+        b = 1.0;
+    }
+
+    // Apply gamma correction
+    r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
+    g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
+    b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
+
+    if (r > b && r > g) {
+        // red is biggest
+        if (r > 1.0) {
+            g = g / r;
+            b = b / r;
+            r = 1.0;
+        }
+    }
+    else if (g > b && g > r) {
+        // green is biggest
+        if (g > 1.0) {
+            r = r / g;
+            b = b / g;
+            g = 1.0;
+        }
+    }
+    else if (b > r && b > g) {
+        // blue is biggest
+        if (b > 1.0) {
+            r = r / b;
+            g = g / b;
+            b = 1.0;
+        }
+    }
+
+    if (r<0) {
+        r = 0.0;
+    }
+    if (g<0) {
+        g = 0.0;
+    }
+    if (b<0) {
+        b = 0.0;
+    }
+
+    return {'r': r, 'g': g, 'b': b};
+}
+
+function checkPointInLampsReach(xyP, cP) {
+    var v1 = {'x': cP.g.x - cP.r.x, 'y': cP.g.y - cP.r.y};
+    var v2 = {'x': cP.b.x - cP.r.x, 'y': cP.b.y - cP.r.y};
+    var q =  {'x': xyP.x - cP.r.x, 'y': xyP.y - cP.r.y};
+    var s = crossProduct(q,v2) / crossProduct(v1,v2);
+    var t = crossProduct(v1,q) / crossProduct(v1,v2);
+    if ( (s >= 0.0) && (t >= 0.0) && (s+t <= 1.0) ) {
+        return true;
+    }
+    return false;
+}
+
+function crossProduct(p1,p2) {
+    return (p1.x*p2.y - p1.y*p2.x);
+}
+
+function getClosestPointToPoints(a,b,p){
+    var AP = {'x': p.x-a.x, 'y': p.y-a.y};
+    var AB = {'x': b.x-a.x, 'y': b.y-a.y};
+    var ab2 = AB.x*AB.x + AB.y*AB.y;
+    var ap_ab = AP.x*AB.x + AP.y*AB.y;
+    var t = ap_ab / ab2;
+    if (t < 0.0) {
+        t = 0.0;
+    }
+    else if (t > 1.0) {
+        t = 1.0;
+    }
+
+    return {'x': a.x - AB.x*t, 'y': a.y + AB.y * t};
+}
+
+function getDistanceBetweenTwoPoints(one,two) {
+    var dx = one.x - two.x;
+    var dy = one.y - two.y;
+    var dist = Math.sqrt(dx*dx + dy*dy);
+    return dist;
+}
+
