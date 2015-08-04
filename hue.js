02a8cc2a7772f35b17dccde2633907155ac677c5
Patrick <patrickmo@gmx.de>
2015-08-04 11:06:58 +0200

    debug write back

--- hue.js
+++ hue.js
@@ -323,7 +323,7 @@ adapter.on('stateChange', function (id, state) {
                     //write back known states
                     for (var finalState in finalLS) {
                         if (finalState in alls){
-                            adapter.log.debug('writing state "' + [id, finalState].join('.') + '" : ' + finalLS[finalState]);
+                            adapter.log.info('writing state "' + [id, finalState].join('.') + '" : ' + finalLS[finalState]);
                             adapter.setState([id, finalState].join('.'), {val: finalLS[finalState], ack: true});
                         }
                     }
