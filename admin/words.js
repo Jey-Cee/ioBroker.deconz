30067800427c9bb83cdcfed1004ff94195610b4f
Jey-Cee <christoph@von-gerle.de>
2017-11-11 00:49:06 +0100

    (Jey-Cee) first release

--- admin/words.js
+++ admin/words.js
@@ -1,42 +1,42 @@
 systemDictionary = {
-    "Philips HUE adapter settings": {
-        "en": "Philips HUE adapter settings",
-        "de": "Philips HUE adapter settings",
-        "ru": "Настройки драйвера Philips HUE"
+    "deConz adapter settings": {
+        "en": "deConz adapter settings",
+        "de": "deConz adapter settings",
+        "ru": "Настройки драйвера deConz"
     },
     "Settings":         {"en": "Settings",       "de": "Einstellungen",    "ru": "Настройки"},
     "Auto setup":       {"en": "Auto setup",     "de": "Auto-Setup",       "ru": "Авто настройка"},
-    "Bridge Address":   {"en": "Bridge Address", "de": "Bridge-Adresse",   "ru": "Адрес HUE bridge"},
+    "deConz Address":   {"en": "deConz Address", "de": "deConz Adresse",   "ru": "Адрес deConz"},
     "Bridge port":      {"en": "Bridge port",    "de": "Bridge-Port",      "ru": "Порт HUE bridge"},
-    "Bridge User":      {"en": "Bridge User",    "de": "Bridge-Anwender",  "ru": "Пользователь hue bridge"},
+    "API key":          {"en": "API key",    "de": "API Key",  "ru": "Ключ API"},
     "Polling":          {"en": "Polling",        "de": "Polling",          "ru": "Опрос"},
     "Polling interval (sec)": {
         "en": "Polling interval(sec)",
         "de": "Pollintervall(sek)",
         "ru": "Интервал опроса(сек)"
     },
-    "Find Bridge":      {"en": "Find Bridge",    "de": "Suche Bridge",     "ru": "Найти HUE bridge"},
-    "Create User":      {"en": "Create User",    "de": "Erstelle User",    "ru": "Создать пользователя"},
-    "Enable adapter first": {"en": "Enable adapter first", "de": "Aktivier Adapter zuerst", "ru": "Сначала надо активировать драйвер"},
+    "Find deConz":      {"en": "Find deConz",    "de": "Suche deConz",     "ru": "Найти deConz"},
+    "Create API key":      {"en": "Create API key",    "de": "Erstelle API key",    "ru": "Создать ключ API"},
+    "Enable adapter first": {"en": "Enable adapter first", "de": "Zu erst den Adapter Aktivieren", "ru": "Сначала надо активировать драйвер"},
     "Ok":               {"en": "Ok",            "de": "Ok",                 "ru": "Ok"},
     "Instruction":      {
-        en: 'Please press link button now! <br />You have 30 seconds.',
-        de: 'Bitte Link unter dem Knopf klicken! <br />Sie haben 30 Sekunden.',
-        ru: 'Нажмите ссылку снизу кнопки в течении следующих 30 секунд.'
+        en: 'Please click "Unlock Gateway" now! <br />You have 30 seconds.',
+        de: 'Bitte Klicken sie jetzt auf "Unlock Gateway"! <br />Sie haben 30 Sekunden.',
+        ru: 'Нажмите «Разблокировать шлюз» прямо сейчас! <br /> У вас есть 30 секунд.'
     },
-    "'Please wait, searching for bridge.'":      {
-        en: 'Please wait, searching for bridge.',
-        de: 'Suche Bridge. Bitte warten...',
-        ru: 'Ищем устройство. Подождите... '
+    "'Please wait, searching for deConz...'":      {
+        en: 'Please wait, searching for deConz...',
+        de: 'Suche deConz. Bitte warten...',
+        ru: 'Подождите, ищите deConz... '
     },
-    "error creating user: ":      {
-        en: 'error creating user: ',
-        de: 'Fehler beim Anwendererzeugen: ',
-        ru: 'Ошибка создания пользователя: '
+    "error creating API key: ":      {
+        en: 'error creating API key: ',
+        de: 'Fehler beim API key erstellen: ',
+        ru: 'ошибка создания ключа API: '
     },
     "link button not pressed":      {
-        en: 'link button not pressed',
-        de: 'Link-Knopf nicht gedruckt',
-        ru: 'Кнопка для соединения не нажата.'
+        en: 'Please first click on "Unlock Gateway" in the web app',
+        de: 'Bitte zuerst "Unlock Gateway" in der Web App Klicken',
+        ru: 'Сначала нажмите «Разблокировать шлюз» в веб-приложении.'
     }
 };
