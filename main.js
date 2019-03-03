e31f215918c11bb1cfb7df3300a31de23aef9535
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:43:36 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const utils     = require(__dirname + '/lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const request = require('request');
 
 const adapter   = new utils.Adapter('deconz');
